c6ba2806e4b785942238b0ea6ee8a0211414ac37
deastyjuli <deasty.juliantysalim@gmail.com>
2026-05-12 10:52:53 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:fasum/firebase_options.dart';
+import 'package:fasum/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
